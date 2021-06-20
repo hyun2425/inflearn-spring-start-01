4ec4d8ba592462232c292eddaa44f1f1d5883f14
sangju <hyun24.dev@gmail.com>
2021-06-20 20:42:17 +0900

    first commit

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,12 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class SpringConfig {
+
+}
